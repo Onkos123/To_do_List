c5f1cad651c1b84e36d055c849c01d181962110e
Onkos123 <theylonfeitosa@gmail.com>
2022-02-04 13:49:23 -0300

    Initial commit

---
+++ app/src/main/java/br/keneitec/dio/todolist/add_task_activity.kt
@@ -0,0 +1,11 @@
+package br.keneitec.dio.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class add_task_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task)
+    }
+}
